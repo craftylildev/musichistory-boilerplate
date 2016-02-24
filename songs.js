1a70b64b64279bf09d113edf12a039febf64c376
Jen Solima <jensolima@gmail.com>
2016-02-24 14:48:38 -0600

    xhr, json, delete btn - part 1 complete

--- songs.js
+++ songs.js
@@ -1,22 +1,3 @@
-// var songs = [];
-
-// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-// songs[songs.length] = "Tuesday's Gone > by Lynyrd Skynyrd on the Album on the album Pronunced";
-
-// songs.unshift("Abracadabra > by Steve Miller Band on the album Abracadabra");
-
-// for (var i = 0; i < songs.length; i++) {
-// 	songs[i] = songs[i].replace("!", "");
-// 	songs[i] = songs[i].replace("*", "");
-// 	songs[i] = songs[i].replace("@", "");
-// 	songs[i] = songs[i].replace("(", "");
-// 	songs[i] = songs[i].replace(">", "-");
-//     document.getElementById("newSongs").innerHTML += "<li>" + songs[i] + "</li>";
-// };
 
 //ADD NEW SONGS
 document.getElementById("add-button").addEventListener("click", function() {
@@ -26,6 +7,62 @@ document.getElementById("add-button").addEventListener("click", function() {
   var inputAlbum = document.getElementById("input-album").value;
   // add new songs/artists/albums
   var newSongs = document.getElementById("new-songs");
-  newSongs.innerHTML += '<li><p class="list-song">' + inputSong + ' </p><span class="list-artist">' + inputArtist + ' </span><span class="list-album">' + inputAlbum + ' </span></li>';
 
+  newSongs.innerHTML += 
+  `<li>
+    <p class="list-song">${inputSong}</p>
+    <span class="list-artist">${inputArtist}</span>
+    <span class="list-album">${inputAlbum}</span>          
+    <button id="deleteTrack" class="deleteTrack">x</button>
+  </li>`;
 });
+
+document.querySelector("body").addEventListener("click", function(event) {
+  // console.log(event);
+  if (event.target.className === "deleteTrack") {
+    // console.log(event.target.parentNode);
+    event.target.parentNode.remove();
+  }
+});
+
+function loadPlayList () {
+  var playListData = JSON.parse(this.responseText);
+  // console.log("playListData", playListData.track_list);
+  var defaultOutput = document.getElementById("default-songs");  
+  var defaultPlayList = "";
+
+  for (var i = 0; i < playListData.track_list.length; i++){
+    // console.log("playListData", playListData.track_list[i]);
+    var currentTrack = playListData.track_list[i];
+    defaultPlayList +=     
+    `<li>
+      <p class="list-song">${currentTrack.song}</p>
+      <span class="list-artist">${currentTrack.artist}</span>
+      <span class="list-album">${currentTrack.album}</span>
+      <span class="list-genre">${currentTrack.genre}</span>
+      <button id="deleteTrack" class="deleteTrack">x</button>
+    </li>`;    
+  }
+  defaultOutput.innerHTML = defaultPlayList;
+}
+
+var playListLoader = new XMLHttpRequest();
+playListLoader.addEventListener("load", loadPlayList);
+playListLoader.open("GET", "songList1.json");
+playListLoader.send();
+
+var loadMoreTracks = document.getElementById("loadMoreTracks");
+
+
+loadMoreTracks.addEventListener("click",  function loadPlayList2 () {
+  console.log("test");
+//   var playListLoader2 = new XMLHttpRequest();
+
+//   playListLoader2.addEventListener("load", function () {
+//     inventory = JSON.parse(this.responseText);
+//   });
+//   playListLoader2.open("GET", "songList2.json");
+//   playListLoader2.send();
+// }
+
+  });
